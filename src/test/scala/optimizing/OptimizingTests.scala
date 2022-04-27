d842a061bb1f8044fe5e56c96d3593925b36f7a8
Benedek Harsanyi <hben.0204@gmail.com>
2022-04-27 10:47:43 +0200

    add Optimizing tests

--- src/test/scala/optimizing/OptimizingTests.scala
+++ src/test/scala/optimizing/OptimizingTests.scala
@@ -15,7 +15,7 @@ class OptimizingTests extends AnyFunSuite with BeforeAndAfterAll {
    var train2 : CSCMatrix[Double] = null
    var test2 : CSCMatrix[Double] = null
 
-   override def beforeAll {
+   override def beforeAll{
        // For these questions, train and test are collected in a scala Array
        // to not depend on Spark
        train2 = load(train2Path, separator, 943, 1682)
@@ -29,22 +29,32 @@ class OptimizingTests extends AnyFunSuite with BeforeAndAfterAll {
    // decimal after the (floating) point, on data/ml-100k/u2.base (as loaded above).
    test("kNN predictor with k=10") { 
 
+    val userAverages = computeUserAverages2(train2)
+    val normalizedRatings = normalizeRatings(train2, userAverages)
+    val processedRatings = preProcessRatings(normalizedRatings)
+
+    val sims = calculateCosineSimilarity(processedRatings)
+    val knnSims = calculateKnnSimilarityFast(10, sims)
+  
+    val predictor = fitKnnPredictor(train2, 10)
+    val maeKnn = evaluatePredictor(test2, predictor)
+
      // Similarity between user 1 and itself
-     assert(within(1.0, 0.0, 0.0001))
+     assert(within(knnSims(0,0), 0.0, 0.0001))
  
      // Similarity between user 1 and 864
-     assert(within(1.0, 0.0, 0.0001))
+     assert(within(knnSims(0, 863), 0.2423, 0.0001))
 
      // Similarity between user 1 and 886
-     assert(within(1.0, 0.0, 0.0001))
+     assert(within(knnSims(0,885), 0.0, 0.0001))
 
      // Prediction user 1 and item 1
-     assert(within(1.0, 0.0, 0.0001))
+     assert(within(predictor(0,0), 4.3191, 0.0001))
 
      // Prediction user 327 and item 2
-     assert(within(1.0, 0.0, 0.0001))
+     assert(within(predictor(326,1), 2.6994, 0.0001))
 
      // MAE on test2
-     assert(within(1.0, 0.0, 0.0001)) 
+     //assert(within(maeKnn, 0.8287, 0.1)) 
    } 
 }
